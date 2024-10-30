1050504e2ef79315ec70d6e6dcadb899baf1c8eb
Rohan Arora <rohanaroraaashi@gmail.com>
2024-10-30 23:58:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,7 @@ import urllib.parse
 app = Flask(__name__)
 
 # Facebook app credentials (ensure these are set as environment variables for security)
-# FB_CLIENT_ID = "1060170592495587"
-# FB_CLIENT_SECRET = "3086cf79fac4df9b9de6219434e6bf17"
-# FB_REDIRECT_URI = "https://your-render-app-url.com/facebook-callback"
+FB_REDIRECT_URI = "https://testingflaskapp-bbys.onrender.com/facebook-callback"
 
 # Start Facebook Login
 @app.route("/facebook-login", methods=["GET"])
